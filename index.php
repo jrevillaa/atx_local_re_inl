eb85f8dd93d0f8968c3e344043523cb090404727
Sail3 <mlosail3@gmail.com>
2016-08-10 16:10:43 -0500

    Newpdf

--- index.php
+++ index.php
@@ -17,7 +17,8 @@ class PDF extends FPDF
         $this->SetFont('','B');
         // Header
         $w = $long;
-        $this->SetX($x);
+        $this->SetX(57);
+        $this->Cell(40,4,utf8_decode('MÓDULO'),0,0,'C');
         $this->SetLineWidth(.5);
         for($i=0;$i<count($header);$i++)
             $this->Cell($w[$i],7,utf8_decode($header[$i]),1,0,'C');
@@ -26,7 +27,7 @@ class PDF extends FPDF
         // Color and font restoration
         $this->SetFillColor(198,224,180);
         $this->SetTextColor(0);
-        
+
         $this->SetFont('');
         // Data
         $fill = true;
@@ -34,10 +35,10 @@ class PDF extends FPDF
         foreach($data as $row)
         {
             $this->SetX($x);
-            
-            for ($i=0; $i <count($w) ; $i++) { 
+
+            for ($i=0; $i <count($w) ; $i++) {
                 $this->Cell($w[$i],6,utf8_decode($row[$i]),'LR',0,'C',true);
-                
+
             }
             //$this->Cell($w[1],6,utf8_decode($row[1]),'LR',0,'L',$fill);
             //$this->Cell($w[2],6,utf8_decode($row[2]),'LR',0,'R',$fill);
@@ -59,7 +60,7 @@ class PDF extends FPDF
         $this->SetFont('','B');
         // Header
         $w = $long;
-        $this->SetX($x);
+        $this->Cell(45,4,utf8_decode('MÓDULO'),0,0,'C');
         $this->SetLineWidth(.5);
         for($i=0;$i<count($header);$i++)
             $this->Cell($w[$i],7,utf8_decode($header[$i]),1,0,'C');
@@ -68,7 +69,7 @@ class PDF extends FPDF
         // Color and font restoration
         $this->SetFillColor(198,224,180);
         $this->SetTextColor(0);
-        
+
         $this->SetFont('');
         // Data
         $fill = true;
@@ -77,7 +78,7 @@ class PDF extends FPDF
         for ($j=0; $j <count($data) ; $j++)
         {
             $this->SetX($x);
-            
+
             for ($i=0; $i <count($w) ; $i++) {
                 if($i+1 == count($w)){
                     $this->SetFillColor(180,198,231);
@@ -91,7 +92,7 @@ class PDF extends FPDF
                     $this->Cell($w[$i],6,utf8_decode($data[$j][$i]),'LR',0,'C',true);
                 }
                 $this->SetFont('');
-                
+
             }
 
             $this->Ln();
@@ -116,46 +117,55 @@ class PDF extends FPDF
         $this->SetLineWidth(.5);
 
         $this->Cell(20.5,4,utf8_decode(''),'LTR',0,'C');
-        $this->Cell(41,4,utf8_decode('Promedio de particitación'),1,0,'C');
-        $this->Cell(41,4,utf8_decode('Curso Virtual'),1,0,'C');
-        $this->Cell(41,4,utf8_decode('Examen de Módulo'),1,0,'C');
+        $this->Cell(41,4,utf8_decode('Promedio de participación'),'LTR',0,'C');
+        $this->Cell(41,4,utf8_decode('Curso Virtual de '),'LTR',0,'C');
+        $this->Cell(41,4,utf8_decode('Examen de Módulo'),'LTR',0,'C');
+        $this->Ln();
+        $this->SetX($x);
+        $this->Cell(20.5,4,utf8_decode(''),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode('de módulo'),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode('Harvard'),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode(''),'LR',0,'C');
         $this->Ln();
 
-        $this->SetX($x);
+        $this->Cell(45,4,utf8_decode('MÓDULO'),0,0,'C');
         $this->Cell(20.5,4,utf8_decode('Nota Final'),'LR',0,'C');
         $this->Cell(41,4,utf8_decode('30%'),1,0,'C');
         $this->Cell(41,4,utf8_decode('40%'),1,0,'C');
         $this->Cell(41,4,utf8_decode('30%'),1,0,'C');
         $this->Ln();
-        $this->SetXY($x,171+$y);
+
+
+      $this->SetXY($x,175+$y);
+
         $this->SetTextColor(255);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),'LBR','C');
-        $this->SetXY(75.5,171+$y);
+        $this->SetXY(75.5,175+$y);
         $this->SetTextColor(0);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
-        $this->SetXY(96,171+$y);
+        $this->SetXY(96,175+$y);
         $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
-        $this->SetXY(116.5,171+$y);
+        $this->SetXY(116.5,175+$y);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
-        $this->SetXY(137,171+$y);
+        $this->SetXY(137,175+$y);
         $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
-        $this->SetXY(157.5,171+$y);
+        $this->SetXY(157.5,175+$y);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
-        $this->SetXY(178,171+$y);
+        $this->SetXY(178,175+$y);
         $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
 
         $this->SetFillColor(198,224,180);
         $this->SetTextColor(0);
-        
+
         $this->SetFont('Arial','',11);
 
         for ($j=0; $j <count($data) ; $j++)
         {
             $this->SetX($x);
-            
+
             $fill = false;
             for ($i=0; $i <count($w) ; $i++) {
-                
+
                 if($fill){
                     $this->SetFillColor(180,198,231);
                 }else{
@@ -164,20 +174,20 @@ class PDF extends FPDF
 
                 $this->Cell($w[$i],6,utf8_decode($data[$j][$i]),'LR',0,'C',true);
                 $this->SetFont('');
-                
+
                 if( $i != 0){
                     $fill = !$fill;
                 }
-                
+
             }
-  
+
             $this->Ln();
         }
 
         $this->SetX($x);
         $this->Cell(array_sum($w),0,'','T');
     }
-
+/*
     function TableFour($data,$x,$long,$y)
     {
 
@@ -220,16 +230,16 @@ class PDF extends FPDF
 
         $this->SetFillColor(198,224,180);
         $this->SetTextColor(0);
-        
+
         $this->SetFont('Arial','',11);
 
         for ($j=0; $j <count($data) ; $j++)
         {
             $this->SetX($x);
-            
+
             $fill = false;
             for ($i=0; $i <count($w) ; $i++) {
-                
+
                 if($fill){
                     $this->SetFillColor(180,198,231);
                 }else{
@@ -238,17 +248,17 @@ class PDF extends FPDF
 
                 $this->Cell($w[$i],6,utf8_decode($data[$j][$i]),'LR',0,'C',true);
                 $this->SetFont('');
-                
+
                 if( $i != 0){
                     $fill = !$fill;
                 }
-                
+
             }
             $this->Ln();
         }
         $this->SetX($x);
         $this->Cell(array_sum($w),0,'','T');
-    }
+    }*/
 
     function TableFive($data,$x,$long,$y)
     {
@@ -264,42 +274,63 @@ class PDF extends FPDF
         $this->SetLineWidth(.5);
 
         $this->Cell(20.5,4,utf8_decode(''),'LTR',0,'C');
-        $this->Cell(41,4,utf8_decode('Reto de liderazgo 1'),1,0,'C');
-        //$this->Cell(20.5,4,utf8_decode('uno'),1,0,'C');
-        $this->Cell(41,4,utf8_decode('Reto de liderazgo 2'),1,0,'C');
-        //$this->Cell(20.5,4,utf8_decode('uno'),1,0,'C');
-        $this->Cell(41,4,utf8_decode('Reto de liderazgo 3'),1,0,'C');
-
+        $this->Cell(41,4,utf8_decode('Participación Casos'),'LTR',0,'C');
+        $this->Cell(41,4,utf8_decode('Control de Lectura '),'LTR',0,'C');
+        $this->Cell(41,4,utf8_decode('Participación Dasafio'),'LTR',0,'C');
+        $this->Cell(41,4,utf8_decode('Trabajo Final'),'LTR',0,'C');
         $this->Ln();
-        $this->SetXY($x,234.2+$y);
-        //$this->SetTextColor(255);
-        $this->MultiCell(20.5,8,utf8_decode('Nota Final'),'LBR','C');
-        $this->SetXY(75.5,234.2+$y);
-        //$this->SetTextColor(0);
+        $this->SetX($x);
+        $this->Cell(20.5,4,utf8_decode(''),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode('Intercorp'),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode(''),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode('Estratégico'),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode(''),'LR',0,'C');
+        $this->Ln();
+
+        $this->SetX($x);
+        $this->Cell(20.5,4,utf8_decode('Nota Final'),'LR',0,'C');
+        $this->Cell(41,4,utf8_decode('20%'),1,0,'C');
+        $this->Cell(41,4,utf8_decode('10%'),1,0,'C');
+        $this->Cell(41,4,utf8_decode('20%'),1,0,'C');
+        $this->Cell(41,4,utf8_decode('50%'),1,0,'C');
+        $this->Ln();
+
+      /*  $this->Ln();*/
+        $this->SetXY($x,234.2+$y+7.5);
+        $this->SetTextColor(255);
+        $this->MultiCell(20.5,8.5,utf8_decode('Nota Final'),'LBR','C');
+        $this->SetXY(40.5,242.2+$y);
+        $this->SetTextColor(0);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
-        $this->SetXY(96,234.2+$y);
+        $this->SetXY(61,242.2+$y);
         $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
-        $this->SetXY(116.5,234.2+$y);
+        $this->SetXY(81.5,242.2+$y);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
-        $this->SetXY(137,234.2+$y);
+        $this->SetXY(102,242.2+$y);
         $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
-        $this->SetXY(157.5,234.2+$y);
+        $this->SetXY(122.5,242.2+$y);
         $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
-        $this->SetXY(178,234.2+$y);
+        $this->SetXY(143,242.2+$y);
         $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
+        $this->SetXY(163.5,242.2+$y);
+        $this->MultiCell(20.5,4,utf8_decode('Nota Participante'),1,'C');
+        $this->SetXY(184,242.2+$y);
+        $this->MultiCell(20.5,4,utf8_decode('Promedio Grupo'),1,'C');
+
+
         //$this->Ln();
 
         $this->SetFillColor(198,224,180);
         $this->SetTextColor(0);
-        
+
         $this->SetFont('Arial','',11);
         for ($j=0; $j <count($data) ; $j++)
         {
             $this->SetX($x);
-            
+
             $fill = false;
             for ($i=0; $i <count($w) ; $i++) {
-                
+
                 if($fill){
                     $this->SetFillColor(180,198,231);
                 }else{
@@ -308,11 +339,11 @@ class PDF extends FPDF
 
                 $this->Cell($w[$i],6,utf8_decode($data[$j][$i]),'LR',0,'C',true);
                 $this->SetFont('');
-                
+
                 if( $i != 0){
                     $fill = !$fill;
                 }
-                
+
             }
 
             $this->Ln();
@@ -396,13 +427,13 @@ $pdf->MultiCell(40,4,utf8_decode($reporte_user),0,'C',false);
 
 
 //*********************        ASISTENCIA      ********************************//
-$pdf->SetXY(58,54);
+$pdf->SetXY(58,50);
 $pdf->Cell(40,6,utf8_decode('I. ASISTENCIA'),0,1,'L');
 
 $pdf->SetFillColor(198,224,180);
 $pdf->SetFont('Arial','',11);
 $pdf->SetXY(58,66);
-$pdf->SetLineWidth(.5);
+$pdf->SetLineWidth(.10);
 $pdf->Cell(40,6,utf8_decode('Módulo Líder'),'LTR',0,'C',true);
 $pdf->SetXY(58,72);
 $pdf->Cell(40,6,utf8_decode('Módulo Colaborador'),'LR',0,'C',true);
@@ -413,7 +444,7 @@ $pdf->Cell(40,6,utf8_decode('Módulo integrador'),'LBR',0,'C',true);
 //$this->SetLineWidth(.5);
 $pdf->SetY(59);
 $header = array('DURACIÓN (Hrs)', 'ASISTENCIA (Hrs)', 'ASISTENCIA');
-$data = array( array(24,24,'100%') , array(16,16,'100%') , array(16,'Pendiente','Pendiente') , array(8,'Pendiente','Pendiente'));
+$data = array( array(16,16,'100%') , array(20,20,'100%') , array(20,'Pendiente','Pendiente') , array(16,'Pendiente','Pendiente'));
 $long = array(35, 38, 29);
 $pdf->TableOne($header,$data,97,$long);
 //*********************        ASISTENCIA      ********************************//
@@ -425,7 +456,7 @@ $pdf->SetXY(10,97+$y+$cuadroDos);
 $pdf->Cell(40,6,utf8_decode('II. CONSOLIDADO DE NOTAS'),0,1,'L');
 //$pdf->SetY(58);
 $header = array('Nota final del módulo', 'Promedio del Grupo 3 Discovery *');
-$data = array( array(17.00,16.75) , array(17.27,16.53) , array('Pendiente','Pendiente') , array('Pendiente','Pendiente') , array('Pendiente','Pendiente') , array('Pendiente','Pendiente'));
+$data = array( array(17.00,16.75) , array(17.27,16.53) , array('Pendiente','Pendiente') , array('Pendiente','Pendiente') ,  array('Pendiente','Pendiente'));
 $long = array(72, 72);
 $pdf->TableTwo($header,$data,55,$long,$cuadroDos);
 
@@ -440,11 +471,21 @@ $pdf->SetXY(10,122+$y+$cuadroDos);
 $pdf->Cell(45,6,utf8_decode('Módulo Negocio'),'LR',0,'C',true);
 $pdf->SetXY(10,128+$y+$cuadroDos);
 $pdf->Cell(45,6,utf8_decode('Módulo integrador'),'LR',0,'C',true);
+//$pdf->SetXY(10,134+$y+$cuadroDos);
+//$pdf->Cell(45,6,utf8_decode('Retos de liderazgo'),'LR',0,'C',true);
 $pdf->SetXY(10,134+$y+$cuadroDos);
-$pdf->Cell(45,6,utf8_decode('Retos de liderazgo'),'LR',0,'C',true);
-$pdf->SetXY(10,140+$y+$cuadroDos);
 $pdf->SetFont('Arial','B',11);
-$pdf->Cell(45,6,utf8_decode('Nota Final'),1,0,'C',false);
+$pdf->SetFillColor(255,255,255);
+$pdf->Cell(45,6,utf8_decode('Nota Final de Programa'),'LTR',1,0,'C',false);
+$pdf->SetXY(10,140+$y+$cuadroDos);
+$pdf->SetFont('Arial','',8);
+$pdf->SetFillColor(255,255,255);
+$pdf->Cell(45,4,utf8_decode('(Promedio simple)'),'LBR',1,1,'L',false);
+$pdf->SetXY(55,140+$y+$cuadroDos);
+$pdf->Cell(72,4,utf8_decode(''),'LBR',1,1,'L',false);
+$pdf->SetXY(127,140+$y+$cuadroDos);
+$pdf->Cell(72,4,utf8_decode(''),'LBR',1,1,'L',false);
+
 $pdf->SetXY(10,146+$y+$cuadroDos);
 $pdf->SetFont('Arial','',7);
 $pdf->SetFillColor(180,198,231);
@@ -465,18 +506,24 @@ $pdf->TableThree($data,55,$long, $y+$cuadroTres);
 
 $pdf->SetFillColor(198,224,180);
 $pdf->SetFont('Arial','',11);
-$pdf->SetXY(10,179+$y+$cuadroTres);
+$pdf->SetXY(10,183+$y+$cuadroTres);
 $pdf->SetLineWidth(.5);
 $pdf->Cell(45,6,utf8_decode('Módulo Líder'),'LTR',0,'C',true);
+$pdf->SetXY(10,189+$y+$cuadroTres);
+$pdf->Cell(45,6,utf8_decode('Módulo Colaborador'),'LR',0,'C',true);
+$pdf->SetXY(10,195+$y+$cuadroTres);
+$pdf->Cell(45,6,utf8_decode('Módulo Negocio'),'LBR',0,'C',true);
+/*
 $pdf->SetXY(10,185+$y+$cuadroTres);
 $pdf->Cell(45,6,utf8_decode('Módulo Colaborador'),'LR',0,'C',true);
 $pdf->SetXY(10,191+$y+$cuadroTres);
 $pdf->Cell(45,6,utf8_decode('Módulo Negocio'),'LBR',0,'C',true);
+*/
 //*********************        DETALLE DE NOTAS POR ACTIVIDAD      ********************************//
 
 
 //*********************        DETALLE DE NOTAS POR ACTIVIDAD      ********************************//
-$pdf->SetFont('Arial','B',11);
+/*$pdf->SetFont('Arial','B',11);
 $pdf->SetXY(10,203+$y+$cuadroCuatro);
 
 $data = array( array('Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente') );
@@ -487,7 +534,7 @@ $pdf->SetFillColor(198,224,180);
 $pdf->SetFont('Arial','',11);
 $pdf->SetXY(10,215+$y+$cuadroCuatro);
 $pdf->SetLineWidth(.5);
-$pdf->Cell(45,6,utf8_decode('Módulo Integrador'),1,0,'C',true);
+$pdf->Cell(45,6,utf8_decode('Módulo Integrador'),1,0,'C',true);*/
 //*********************        DETALLE DE NOTAS POR ACTIVIDAD      ********************************//
 
 
@@ -496,15 +543,15 @@ $pdf->Cell(45,6,utf8_decode('Módulo Integrador'),1,0,'C',true);
 $pdf->SetFont('Arial','B',11);
 $pdf->SetXY(10,230+$y+$cuadroCinco);
 
-$data = array( array('Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente') );
-$long = array(20.5, 20.5,20.5, 20.5, 20.5, 20.5, 20.5);
-$pdf->TableFive($data,55,$long, $y+$cuadroCinco);
+$data = array( array('Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente', 'Pendiente','Pendiente','Pendiente') );
+$long = array(20.5, 20.5,20.5, 20.5, 20.5, 20.5, 20.5, 20.5, 20.5);
+$pdf->TableFive($data,20,$long, $y+$cuadroCinco);
 
 $pdf->SetFillColor(198,224,180);
 $pdf->SetFont('Arial','',11);
-$pdf->SetXY(10,242+$y+$cuadroCinco);
+$pdf->SetXY(2,250.2+$y+$cuadroCinco);
 $pdf->SetLineWidth(.5);
-$pdf->Cell(45,6,utf8_decode('Retos de liderazgo'),1,0,'C',true);
+$pdf->Cell(18,6,utf8_decode('Integral'),1,0,'C',true);
 //*********************        DETALLE DE NOTAS POR ACTIVIDAD      ********************************//
 
 
